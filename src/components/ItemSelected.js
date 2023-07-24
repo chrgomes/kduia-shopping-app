552f22a85a9370102ff0fe361f53671ef805430c
Chris G <chrisgomesdesigns@gmail.com>
2023-07-24 01:39:13 -0400

    Update ItemSelected.js
    Version 1.2

--- src/components/ItemSelected.js
+++ src/components/ItemSelected.js
@@ -75,10 +75,3 @@ const ItemSelected = (props) => {
 };
 
 export default ItemSelected;
-import React, { useContext, useState } from 'react';
-import { AppContext } from '../context/AppContext';
-
-const ItemSelected = (props) => {
-};
-
-export default ItemSelected;
